b90e9e00629c19d52503ff2f34dacef150feb172
Nathaly Sanchez Hincapie <ns7hincapie@gmail.com>
2016-02-17 02:08:44 +0100

    version1

---
+++ main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("numeros del uno al cinco\n");
+    for(x=1;x<=5;x++)
+        printf("\t%d",x);
+
+    return 0;
+}
